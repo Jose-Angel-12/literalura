7d1c687998f9bff4371fe12f2195d7251257eef5
Jose-Angel-12 <jajaraba12@gmail.com>
2025-06-12 12:17:18 -0500

    update

--- src/main/java/com/gelse/literalura/principal/Principal.java
+++ src/main/java/com/gelse/literalura/principal/Principal.java
@@ -5,16 +5,43 @@ import com.gelse.literalura.model.DatosLibros;
 import com.gelse.literalura.service.ConsumoApi;
 import com.gelse.literalura.service.ConvierteDatos;
 
+import java.util.Scanner;
+
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
     private ConsumoApi consumoApi = new ConsumoApi();
     private ConvierteDatos convierteDatos = new ConvierteDatos();
+    private Scanner teclado = new Scanner(System.in);
 
-    public void muestraElMenu(){
-        String datosJson = consumoApi.obtenerDatosApi(URL_BASE);
-        System.out.println("Datos json "  + datosJson);
+    public void muestraElMenu() {
 
-        var datosConvertidos = convierteDatos.obtenerDatos(datosJson, Datos.class);
-        System.out.println(datosConvertidos);
+        var opcion = -1;
+        while (opcion != 0) {
+            var menu = "\n" + """
+                    1 - Buscar Libros 
+                    
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    buscarLibro();
+                    break;
+
+                case 0:
+                    System.out.println("Cerrando la aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
+    }
+
+    private void buscarLibro() {
+
     }
 }
